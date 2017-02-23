351edcc1e7cd941e68950a0a1fec496085b5e3ad
b5710546232 <nattapat.su@ku.th>
2017-02-24 00:53:32 +0700

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,35 @@
+import { Component, ViewChild } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+declare var google;
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  @ViewChild('map') mapElement;
+  map:any;
+  constructor(public navCtrl: NavController) {
+    
+  }
+
+  ionViewDidLoad(){
+    this.initMap();
+  }
+
+  initMap(){
+    let latLng = new google.maps.LatLng(15.8700, 100.9925);
+
+    let mapOptions = {
+      center:latLng,
+      zoom:5,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement,mapOptions);
+  }
+
+}
